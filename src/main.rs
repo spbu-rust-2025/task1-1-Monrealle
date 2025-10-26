77782595a26aad78f8d9cb0042c9509379364579
Stanislav Klimovich <stasklimovish@gmail.com>
2025-10-27 01:54:26 +0300

    Realized simple sum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
+use std::io;
 
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    let numbers: Vec<&str> = input.split_whitespace().collect();
+
+    if numbers.len() < 2 {
+        let first: i64 = numbers[0].parse().unwrap();
+        println!("{}", first);
+    } else {
+        let first: i64 = numbers[0].parse().unwrap();
+        let second: i64 = numbers[1].parse().unwrap();
+        let result = first + second;
+        println!("{}", result);
+    }
 }
